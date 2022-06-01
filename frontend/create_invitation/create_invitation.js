d7fc2b4691fca849b2ad232a1e19ff2c36c2d7bc
Angel Marinski <77447043+AngelMarinski@users.noreply.github.com>
2022-06-01 22:45:40 +0300

    add meme generator & reorganize file structure

---
+++ frontend/create_invitation/create_invitation.js
@@ -0,0 +1,59 @@
+const imageFileInput = document.querySelector("#imageFileInput");
+const topText = document.querySelector("#topText");
+const bottomText = document.querySelector("#bottomText");
+const canvas = document.querySelector("#meme");
+
+let image;
+
+imageFileInput.addEventListener("change", () =>{
+    const imageDataUrl = URL.createObjectURL(imageFileInput.files[0]);
+
+    image = new Image();
+    image.src = imageDataUrl;
+
+    image.addEventListener("load", () => {
+        updateMemeCanvas(canvas, image, topText.value, bottomText.value);
+    }, { once: true });
+});
+
+topText.addEventListener("change", () => {
+    updateMemeCanvas(canvas, image, topText.value, bottomText.value);
+})
+
+bottomText.addEventListener("change", () =>{
+    updateMemeCanvas(canvas, image, topText.value, bottomText.value);
+})
+
+function updateMemeCanvas(canvas, image, topText, bottomText){
+    const ctx = canvas.getContext("2d");
+    const width = image.width;
+    const height = image.height;
+
+    const fontSize = Math.floor(width/10);
+    const yOffset = height / 25;
+
+
+    //Update canvas background
+    canvas.width = width;
+    canvas.height = height;
+
+    ctx.drawImage(image, 0, 0);
+
+    // Prepare text
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = Math.floor(fontSize / 4);
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.lineJoin = "round";
+    ctx.font = `${fontSize}px sans-serif`;
+    
+    // Add top text
+    ctx.textBaseline = "top";
+    ctx.strokeText(topText, width / 2, yOffset);
+    ctx.fillText(topText, width / 2, yOffset);
+    
+    //add bottom text
+    ctx.textBaseline = "bottom";
+    ctx.strokeText(bottomText, width/2, height - yOffset);
+    ctx.fillText(bottomText, width/2, height - yOffset);
+}
