626f6fb166cca3ab4be34cc5f3f5c2e0fbc6f4b9
Ayshe Dzhindzhi <adzh2100@gmail.com>
2022-06-12 16:46:11 +0300

    fix seed

--- database/db_seed.sql
+++ database/db_seed.sql
@@ -12,14 +12,14 @@ INSERT INTO `users` (`username`, `password`, `first_name`, `last_name`, `email`,
 INSERT INTO `users` (`username`, `password`, `first_name`, `last_name`, `email`, `specification`, `year`, `academical_number`, `role`)
       VALUES('gsabev', '5f4dcc3b5aa765d61d8327deb882cf99', 'Георги', 'Събев', 'sabev@abv.bg', 'СИ', 3, 62380, 'student');
 
-INSERT INTO `invitations` (`user_id`, `presentation_theme`, `date`, `time`, `description`)
-      VALUES(2, 'CSS Grid', '2022-06-22', '10:30', 'Ще бъде много яко');
+INSERT INTO `invitations` (`user_id`, `presentation_theme`, `date`, `time`, `description`, `auto_generated`)
+      VALUES(2, 'CSS Grid', '2022-06-22', '10:30', 'Ще бъде много яко', 0);
 
-INSERT INTO `invitations` (`user_id`, `presentation_theme`, `date`, `time`, `description`)
-      VALUES(1, 'WebSocket в PHP', '2022-06-22', '09:30', 'Чакам ви!');
+INSERT INTO `invitations` (`user_id`, `presentation_theme`, `date`, `time`, `auto_generated`)
+      VALUES(1, 'WebSocket в PHP', '2022-06-22', '09:30', 'Чакам ви!', 0);
 
-INSERT INTO `invitations` (`user_id`, `presentation_theme`, `date`, `time`, `description`)
-      VALUES(3, 'Object Oriented CSS (OOCSS)', '2022-06-22', '12:30', 'Най-добрия реферат до сега!');
+INSERT INTO `invitations` (`user_id`, `presentation_theme`, `date`, `time`, `description`, `auto_generated`)
+      VALUES(3, 'Object Oriented CSS (OOCSS)', '2022-06-22', '12:30', 'Най-добрия реферат до сега!', 1);
 
-INSERT INTO `invitations` (`user_id`, `presentation_theme`, `date`, `time`, `description`)
-      VALUES(4, 'Javascript Lint', '2022-06-21', '10:30', 'Toва не го знаете, обещавам!');
+INSERT INTO `invitations` (`user_id`, `presentation_theme`, `date`, `time`, `description`, `auto_generated`)
+      VALUES(4, 'Javascript Lint', '2022-06-21', '10:30', 'Toва не го знаете, обещавам!', 1);
