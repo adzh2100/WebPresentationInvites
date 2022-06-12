4fb421618bf7c5eaa7c81115d90911bea54dddf6
Ayshe Dzhindzhi <adzh2100@gmail.com>
2022-06-12 16:56:24 +0300

    fix seed, improve styles

--- database/db_seed.sql
+++ database/db_seed.sql
@@ -15,7 +15,7 @@ INSERT INTO `users` (`username`, `password`, `first_name`, `last_name`, `email`,
 INSERT INTO `invitations` (`user_id`, `presentation_theme`, `date`, `time`, `description`, `auto_generated`)
       VALUES(2, 'CSS Grid', '2022-06-22', '10:30', 'Ще бъде много яко', 0);
 
-INSERT INTO `invitations` (`user_id`, `presentation_theme`, `date`, `time`, `auto_generated`)
+INSERT INTO `invitations` (`user_id`, `presentation_theme`, `date`, `time`, `description`, `auto_generated`)
       VALUES(1, 'WebSocket в PHP', '2022-06-22', '09:30', 'Чакам ви!', 0);
 
 INSERT INTO `invitations` (`user_id`, `presentation_theme`, `date`, `time`, `description`, `auto_generated`)
